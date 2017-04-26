e6f61ce6e6bca553b0dbec553ccadc559a97f46e
sunalong <sunalong@ztgame.com>
2017-04-26 18:41:33 +0800

    简化代码

--- app/src/main/java/com/itcode/asdemo/VideoActivity.java
+++ app/src/main/java/com/itcode/asdemo/VideoActivity.java
@@ -13,16 +13,12 @@ import android.widget.Toast;
 
 import com.itcode.asdemo.ui.SurfaceViewContainer;
 import com.ztgame.videoengine.NativeVideoEngine;
-import com.ztgame.videoengine.VideoEngineImpl;
 import com.ztgame.voiceengine.NativeVoiceEngine;
 
 public class VideoActivity extends Activity {
 
-    public static final boolean NATIVETEST = true;
-
     private CheckBox viewSomeOneCheckBox;
     private CheckBox openRemoteVideoCheckBox;
-    private CheckBox displayRemoteViewCheckBox;
     private CheckBox sendLocalVideoCheckBox;
     private CheckBox displayLocalViewCheckBox;
     private CheckBox cameraSwitchCheckBox;
@@ -41,7 +37,6 @@ public class VideoActivity extends Activity {
     private boolean blouderspeak = false;
     private boolean bunmute = true;
 
-    private VideoEngineImpl mVideoEngineImp;
     private NativeVideoEngine mNVEngine;
 
     enum enVideoSource
@@ -56,37 +51,13 @@ public class VideoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-
         final Intent intent = getIntent();
 
         username = intent.getStringExtra("username");
         //init nativeVideoEngine
-        mVideoEngineImp = VideoEngineImpl.getInstance();
         mNVEngine = NativeVideoEngine.getInstance();
-        if(NATIVETEST)
             mNVEngine.Init(this);
-        else
-            mVideoEngineImp.Init(this);
-        //init surface view
-        localSurfaceViewContainer = (RelativeLayout) findViewById(R.id.surfaceViewLocalContainer);
-        remoteSurfaceViewContainer = (RelativeLayout) findViewById(R.id.surfaceViewRemoteContainer);
-        glLocalSurfaceViewContainer = new SurfaceViewContainer(this);
-        glRemoteSurfaceViewContainer = new SurfaceViewContainer(this);
-        //add view
-        localSurfaceViewContainer.addView(glLocalSurfaceViewContainer);
-        remoteSurfaceViewContainer.addView(glRemoteSurfaceViewContainer);
-
-        sendLocalVideoCheckBox = (CheckBox)findViewById(R.id.checkBox1);
-        displayLocalViewCheckBox = (CheckBox)findViewById(R.id.checkBox2);
-        cameraSwitchCheckBox = (CheckBox)findViewById(R.id.checkBox3);
-
-        openRemoteVideoCheckBox = (CheckBox)findViewById(R.id.checkBox10);
-        displayRemoteViewCheckBox = (CheckBox)findViewById(R.id.checkBox11);
-        viewSomeOneCheckBox = (CheckBox)findViewById(R.id.checkBox12);
-
-        disconnectButton = (ImageButton)findViewById(R.id.button_call_disconnect);
-        toggleMuteButton = (ImageButton)findViewById(R.id.button_call_toggle_mic);
-        toggleSpeakerButton = (ImageButton)findViewById(R.id.button_call_toggle_speak);
+        initView();
 
         //init voice mic and speaker status
         NativeVoiceEngine.getInstance().setLouderSpeaker(blouderspeak);
@@ -116,10 +87,7 @@ public class VideoActivity extends Activity {
         disconnectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(NATIVETEST)
                     mNVEngine.release();
-                else
-                    mVideoEngineImp.release();
                 NativeVoiceEngine.getInstance().requestLeavePlatformRoom();
                 finish();
             }
@@ -130,17 +98,11 @@ public class VideoActivity extends Activity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
                 if(isChecked){
-                    if(NATIVETEST)
                         mNVEngine.startSendVideo();
-                    else
-                        mVideoEngineImp.startSendVideo();
                     Toast.makeText(VideoActivity.this, "发送本地视频", Toast.LENGTH_LONG).show();
                 }
                 else {
-                    if(NATIVETEST)
                         mNVEngine.stopSendVideo();
-                    else
-                        mVideoEngineImp.stopSendLocalVideo();
                     Toast.makeText(VideoActivity.this, "停止发送本地视频", Toast.LENGTH_LONG).show();
                 }
             }
@@ -149,10 +111,7 @@ public class VideoActivity extends Activity {
         displayLocalViewCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(NATIVETEST)
                     mNVEngine.observerLocalVideoWindow(isChecked,glLocalSurfaceViewContainer.getSurfaceView());
-                else
-                    mVideoEngineImp.observerLocalVideo(isChecked,glLocalSurfaceViewContainer.getSurfaceView());
                 SurfaceView mLocalSurfaceView = glLocalSurfaceViewContainer.getSurfaceView();
                 if(mLocalSurfaceView!=null)
                     mLocalSurfaceView.setVisibility(isChecked? View.VISIBLE: View.INVISIBLE);
@@ -164,16 +123,10 @@ public class VideoActivity extends Activity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 //mNativeVideoEngine.switchCamera(!isChecked);
                 if(isChecked) {
-                    if(NATIVETEST)
                         mNVEngine.switchCamera(enVideoSource.kVideoSourceBackCamera.ordinal()); //back
-                    else
-                        mVideoEngineImp.switchCamera(false);
                 }
                 else {
-                    if(NATIVETEST)
                         mNVEngine.switchCamera(enVideoSource.kVideoSourceFrontCamera.ordinal()); //front
-                    else
-                        mVideoEngineImp.switchCamera(true);
                 }
                 Toast.makeText(VideoActivity.this, "切换摄像头中......", Toast.LENGTH_LONG).show();
             }
@@ -183,18 +136,11 @@ public class VideoActivity extends Activity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
-                    if(NATIVETEST)
                         mNVEngine.startObserverRemoteVideo(glRemoteSurfaceViewContainer.getSurfaceView());
-                    else
-                        mVideoEngineImp.startObserverRemoteVideo(glRemoteSurfaceViewContainer.getSurfaceView());
                     Toast.makeText(VideoActivity.this, "打开接受远端视频", Toast.LENGTH_LONG).show();
                 }
                 else {
-                    if(NATIVETEST)
                         mNVEngine.stopObserverRemoteVideo();
-                    else
-                        mVideoEngineImp.stopObserverRemoteVideo();
-
                     Toast.makeText(VideoActivity.this, "关闭接受远端视频", Toast.LENGTH_LONG).show();
                 }
                 SurfaceView mRemoteSurfaceView = glRemoteSurfaceViewContainer.getSurfaceView();
@@ -202,31 +148,17 @@ public class VideoActivity extends Activity {
                     mRemoteSurfaceView.setVisibility(isChecked? View.VISIBLE: View.INVISIBLE);
             }
         });
-
-        /*displayRemoteViewCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                mNativeVideoEngine.displayRemoteView(isChecked);
-            }
-        });*/
-
         viewSomeOneCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 String msg;
                 if(isChecked) {
                     msg = "切换到单人模式观看";
-                    if(NATIVETEST)
                         mNVEngine.observerSomeOneVideo(username);
-                    else
-                        mVideoEngineImp.SwitchSomeOneView(username);
                 }
                 else {
                     msg = "切换到多人模式观看";
-                    if(NATIVETEST)
                         mNVEngine.observerSomeOneVideo("");
-                    else
-                        mVideoEngineImp.SwitchSomeOneView("");
                 }
                 Toast.makeText(VideoActivity.this, msg, Toast.LENGTH_LONG).show();
             }
@@ -235,70 +167,35 @@ public class VideoActivity extends Activity {
 
     }
 
+    private void initView() {
+        //init surface view
+        localSurfaceViewContainer = (RelativeLayout) findViewById(R.id.surfaceViewLocalContainer);
+        remoteSurfaceViewContainer = (RelativeLayout) findViewById(R.id.surfaceViewRemoteContainer);
+        glLocalSurfaceViewContainer = new SurfaceViewContainer(this);
+        glRemoteSurfaceViewContainer = new SurfaceViewContainer(this);
+        //add view
+        localSurfaceViewContainer.addView(glLocalSurfaceViewContainer);
+        remoteSurfaceViewContainer.addView(glRemoteSurfaceViewContainer);
+
+        sendLocalVideoCheckBox = (CheckBox)findViewById(R.id.checkBox1);
+        displayLocalViewCheckBox = (CheckBox)findViewById(R.id.checkBox2);
+        cameraSwitchCheckBox = (CheckBox)findViewById(R.id.checkBox3);
+
+        openRemoteVideoCheckBox = (CheckBox)findViewById(R.id.checkBox10);
+        viewSomeOneCheckBox = (CheckBox)findViewById(R.id.checkBox12);
+
+        disconnectButton = (ImageButton)findViewById(R.id.button_call_disconnect);
+        toggleMuteButton = (ImageButton)findViewById(R.id.button_call_toggle_mic);
+        toggleSpeakerButton = (ImageButton)findViewById(R.id.button_call_toggle_speak);
+    }
+
     @Override
     protected void onDestroy() {
-        if(NATIVETEST) {
             mNVEngine.destroyRenderView(glLocalSurfaceViewContainer.getSurfaceView());
             mNVEngine.destroyRenderView(glRemoteSurfaceViewContainer.getSurfaceView());
             mNVEngine.release();
-        }
-        else {
-            mVideoEngineImp.destroyRenderView(glLocalSurfaceViewContainer.getSurfaceView());
-            mVideoEngineImp.destroyRenderView(glRemoteSurfaceViewContainer.getSurfaceView());
-            mVideoEngineImp.release();
-        }
         NativeVoiceEngine.getInstance().requestLeavePlatformRoom();
         finish();
         super.onDestroy();
     }
-
-    private void updateCheckBoxStatus(boolean status){
-        sendLocalVideoCheckBox.setChecked(false);
-        displayLocalViewCheckBox.setChecked(status);
-        cameraSwitchCheckBox.setChecked(false);
-
-        openRemoteVideoCheckBox.setChecked(false);
-        displayRemoteViewCheckBox.setChecked(false);
-        viewSomeOneCheckBox.setChecked(false);
-    }
-
-    public void jumpOnClick1(View view){
-        switch (view.getId()){
-            case R.id.initButton: {
-                //updateCheckBoxStatus(true);
-               // mNativeVideoEngine.setInitView();
-               // mNativeVideoEngine.setInitView(glLocalSurfaceViewContainer.getSurfaceView(), glRemoteSurfaceViewContainer.getSurfaceView());
-                //NativeVideoEngine1.getInstance().registerPrivate(this);
-
-                break;
-            }
-            case R.id.unInitButton: {
-                //updateCheckBoxStatus(false);
-                //mNativeVideoEngine.release();
-                //mNativeVideoEngine.destroyRenderView(glLocalSurfaceViewContainer.getSurfaceView());
-                //mNativeVideoEngine.destroyRenderView(glRemoteSurfaceViewContainer.getSurfaceView());
-                //mNVEngine.release();
-               // mNVEngine.destroyRenderView(glLocalSurfaceViewContainer.getSurfaceView());
-                //mNVEngine.destroyRenderView(glRemoteSurfaceViewContainer.getSurfaceView());
-                break;
-            }
-
-            /*case R.id.button: {
-                //mNativeVideoEngine.openRemoteVideo(true);
-                break;
-            }
-
-            case R.id.button3: {
-                //mNativeVideoEngine.sendLocalVideo(true);
-                break;
-            }
-
-            case R.id.button4: {
-               // mNativeVideoEngine.sendLocalVideo(true);
-                break;
-            }*/
-        }
-    }
-
-
 }
