c74b14d7658aac42d9a97f86b269fd0598ef74ae
sunalong <sunalong@ztgame.com>
2017-10-10 17:11:51 +0800

    先使用外网测试IP，发布后再使用外网正式IP

--- app/src/main/java/com/ztgametest/MainActivity.java
+++ app/src/main/java/com/ztgametest/MainActivity.java
@@ -93,7 +93,8 @@ public class MainActivity extends Activity implements View.OnClickListener, Radi
     //外网key
     private static final String outerAppId = "3768c59536565afb";
     private static final String outerAppKey = "df191ec457951c35b8796697c204382d0e12d4e8cb56f54df6a54394be74c5fe";
-    private static final String outerPlatformServerUrl = "room.audio.mztgame.com:8080";
+    private static final String outerPlatformServerUrl = "115.159.251.79:8080";//外网测试IP
+//    private static final String outerPlatformServerUrl = "room.audio.mztgame.com:8080";//外网正式IP
 
 
 
